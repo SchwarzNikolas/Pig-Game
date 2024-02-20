397d7d12bab2d303e4f6cbe3afca1f9c0339ce51
SchwarzNikolas <spyr0w@a-schwarz.info>
2024-02-20 18:44:22 +0100

    Added command to set the player amount / create players

--- game/Game.py
+++ game/Game.py
@@ -1,6 +1,36 @@
-class Game():
-    def __init__(self):
-        self.running = True
+from Player import Player
 
-    def play(self):
+
+class Game:
+    def __init__(self):
+        """Initialize Object."""
+        self.players = []
+        self.game_state = None
+        self.amount_players = 0
+
+    def start(self):
+        if (len(self.players) < 1):
+            return False
+        for player in self.players:
+            if (not isinstance(player, Player)):
+                return False
+        self.game_state = 0
+        return True
+
+    def create_player(self, names):
+        if (self.amount_players < 1):
+            return "Please set the amount of players first!"
+        for i in range(self.amount_players):
+            self.players.append(Player(names[i]))
+            print(f"Player {names[i]} has been created!")
+        return "All players have been created!"
+
+    def player_amount(self, num):
+        if (num > 6 or num < 1):
+            raise ValueError("The amount of players must be between 1 and 6.")
+
+        self.amount_players = num
+
+    def roll(self):
+        """Player decides to roll the dice."""
         print("test")
--- game/Output.py
+++ game/Output.py
@@ -12,9 +12,33 @@ class Output(cmd.Cmd):
         super().__init__()
         self.game = Game()
 
-    def do_play(self, _):
-        """Continue playing."""
-        self.game.play()
+    def do_start(self, _):
+        """Start a new game."""
+        if (self.game.start()):
+            print("Game started, player one starts!")
+        else:
+            print("Please identify yourself!")
+
+    def do_create(self, arg):
+        args = arg.split()
+        msg = self.game.create_player(args)
+        print(msg)
+
+    def do_player_amount(self, arg):
+        """Set the amount of players.
+        If set to 1 the player will play against the computer.
+        If set to another value the players will play against each other
+        Maxmimum amount of players is 6"""
+        msg = "Missing argument, provide the amount of players. \
+Try: \"player_amount 1\""
+        if not arg:
+            print(msg)
+            return
+
+        try:
+            msg = self.game.player_amount(int(arg))
+        except (ValueError, TypeError) as err:
+            print(err)
 
     def do_exit(self, _):
         """Exit the game."""
