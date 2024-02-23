2ddba4404f28d45f788683fc7404b32bb0d42f99
StackedFrog <isaac.prins4@gmail.com>
2024-02-23 10:52:47 +0100

    creating classes

---
+++ game/Player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, player_name):
+        self.player_name = player_name
+        self.highscore_Least_rolls = 0 
+        self.highscore_Least_rounds = 0 
+
+    def change_name(self,):
