44af9d3367b13de86d7cf7e51243eb161f19e811
OscarHNilsson <159028523+OscarHNilsson@users.noreply.github.com>
2024-02-23 12:33:35 +0100

    Update Dicehand.py

--- game/Dicehand.py
+++ game/Dicehand.py
@@ -1,6 +1,18 @@
-[InternetShortcut]
-URL=https://github.com/SchwarzNikolas/Pig-Game/blob/main/game/Dicehand.py
-IDList=
-HotKey=0
-IconFile=C:\Users\Oscar\AppData\Local\Mozilla\Firefox\Profiles\wt2fj1et.default-release\shortcutCache\MMbHVSnNkpmqEFzunKyDcQTyV8S7j0s7hAEBmM8r7C0=.ico
-IconIndex=0
+"Using result of rolling dice"
+
+class DiceHand:
+    "Dicehand woohoo"
+
+    def __init__(self):
+        pass
+
+    def updateHand():
+        
+    def getRoundPoints():
+        return
+
+    def getRoundsplayed():
+        return
+
+    def getRollAmount():
+        return
