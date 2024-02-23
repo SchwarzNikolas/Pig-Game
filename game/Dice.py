665d4ea3aa2ad11c67f714348432f4a84fd1462f
Oscar Nilsson <oscarhedbergn@gmail.com>
2024-02-23 14:53:17 +0100

    Cleared up empty lines in Dice.py and added some functionality to the methods in Dicehand.py

--- game/Dice.py
+++ game/Dice.py
@@ -1,12 +1,10 @@
 "We rolling dice baby"
 import random
 
-
 class Dice:
     "This here the dice"
     def __init__(self):
         self.num = 0
         
     def roll(self):
-        return random.randint(1, 6)
-    
+        return random.randint(1, 6)   
--- game/Dicehand.py
+++ game/Dicehand.py
@@ -1,18 +1,27 @@
 "Using result of rolling dice"
 
 class DiceHand:
-    "Dicehand woohoo"
+    "Keeps track of integer variables and returns them with getters"
 
     def __init__(self):
-        pass
+        self.points = 0
+        self.rounds = 0
+        self.rollamount = 0
 
-    def updateHand():
-        
-    def getRoundPoints():
-        return
+    def updateHand(self, diceroll):
+        if diceroll != 1:
+            self.points += diceroll
+            self.rollamount += 1
+        else:
+            "placeholder for sending result to highscore/histogram"
+            self.points = 0
+            self.rounds += 1
 
-    def getRoundsplayed():
-        return
+    def getRoundPoints(self):
+        return self.points
 
-    def getRollAmount():
-        return
+    def getRoundsplayed(self):
+        return self.rounds
+
+    def getRollAmount(self):
+        return self.rollamount
