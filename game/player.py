ccc165466fc881d5ac4a0e15e13817909f666cd4
StackedFrog <isaac.prins4@gmail.com>
2024-02-26 19:17:39 +0100

    skeleton and documentation

--- game/player.py
+++ game/player.py
@@ -1,7 +1,27 @@
 class Player:
     def __init__(self, player_name):
         self.player_name = player_name
-        self.highscore_Least_rolls = 0 
-        self.highscore_Least_rounds = 0 
+        self.highscore_Least_rolls = 0
+        self.highscore_Least_rounds = 0
 
-    def change_name(self,):
+    def create_player():
+        """The Beginning"""
+        pass
+
+    def delete_player():
+        """The End"""
+
+        pass
+
+    def change_name():
+        """Changing Identity"""
+
+        pass
+
+    def roll_dice():
+        """Allows"""
+        pass
+
+    def return_stats():
+
+        pass
