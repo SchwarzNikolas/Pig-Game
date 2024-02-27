7079bc737a96690e352a8ac718a03a394ead9a78
StackedFrog <isaac.prins4@gmail.com>
2024-02-27 17:00:47 +0100

    Making the linters happy

--- game/player.py
+++ game/player.py
@@ -1,27 +1,32 @@
+"""The Module with the Player blueprint."""
+
+from dice import Dice
+from dicehand import DiceHand
+
+
 class Player:
+    """The Blueprint for a Player."""
+
     def __init__(self, player_name):
+        """Beginning of a New Player."""
         self.player_name = player_name
-        self.highscore_Least_rolls = 0
-        self.highscore_Least_rounds = 0
+        self.highscore_least_rolls = 0
+        self.highscore_least_rounds = 0
 
-    def create_player():
-        """The Beginning"""
-        pass
+    def reset_player_stats(self):
+        """Beginning for an Existing Player."""
+        confirmation = input("Proceed with Reset? [y/N] ")
+        confirmation.lower()
+        if confirmation == "y":
+            self.highscore_least_rolls = 0
+            self.highscore_least_rounds = 0
 
-    def delete_player():
-        """The End"""
+    def change_name(self):
+        """Alter the Identity of the Player."""
+        self.player_name = input("Please enter your new player name: ")
 
-        pass
+    def roll_dice(self):
+        """Decides Fate."""
 
-    def change_name():
-        """Changing Identity"""
-
-        pass
-
-    def roll_dice():
-        """Allows"""
-        pass
-
-    def return_stats():
-
-        pass
+    def return_stats(self):
+        """Museum of your previous games."""
