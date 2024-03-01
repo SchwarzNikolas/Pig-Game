e19ab2dd98080dc27dc488573424b620926f0bc8
StackedFrog <isaac.prins4@gmail.com>
2024-03-01 12:03:41 +0100

    intermediate

--- test/test_highscore.py
+++ test/test_highscore.py
@@ -3,10 +3,12 @@
 import unittest
 from game.highscore import Highscore
 
+
 class TestHighscoreClass(unittest.TestCase):
-        """Test the Highscore class."""
-        def test_init_default_object(self):
-            """Instantiate an object and check its properties."""
-            res = Highscore()
-            exp = Highscore
-            self.assertIsInstance(res, exp)
+    """Test the Highscore class."""
+
+    def test_init_default_object(self):
+        """Instantiate an object and check its properties."""
+        res = Highscore()
+        exp = Highscore
+        self.assertIsInstance(res, exp)
--- test/test_player.py
+++ test/test_player.py
@@ -3,26 +3,28 @@
 import unittest
 from game.player import Player
 
+
 class TestPlayerClass(unittest.TestCase):
-        """Test the Player class."""
+    """Test the Player class."""
 
-        def test_init_default_object(self):
-            """Instantiate an object and check its properties."""
-            res = Player()
-            exp = Player
-            self.assertIsInstance(res, exp)
+    def test_init_default_object(self):
+        """Instantiate an object and check its properties."""
+        res = Player()
+        exp = Player
+        self.assertIsInstance(res, exp)
 
-        def test_reset_player_stats(self):
-            """Test if the Player's statistics are reset"""
-            pass
-        
-        def test_change_name(self):
-            """Test if the Player's name changed"""
-            pass
+    def test_reset_player_stats(self):
+        """Test if the Player's statistics are reset."""
+        pass
 
-        def test_roll_dice(self):
-            pass
+    def test_change_name(self):
+        """Test if the Player's name changed."""
+        pass
 
-        def test_return_stats(self):
-            """Test if statistics are shown"""
-            pass
+    def test_roll_dice(self):
+        """Method to validate dice roll."""
+        pass
+
+    def test_return_stats(self):
+        """Test if statistics are shown."""
+        pass
