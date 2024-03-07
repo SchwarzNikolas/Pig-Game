726f7bc65d49fb1f33dc93321d6f9ed7f08681e1
StackedFrog <isaac.prins4@gmail.com>
2024-03-07 16:36:14 +0100

    finalized player class

--- game/player.py
+++ game/player.py
@@ -2,6 +2,7 @@
 
 from game.dice import Dice
 from game.dicehand import DiceHand
+from game.highscore import Highscore
 
 
 class Player:
@@ -11,16 +12,14 @@ class Player:
         """Beginning of a new Player."""
         self.player_name = player_name
         self.dice_holder = DiceHand()
-        self.highscore_least_rolls = 0
-        self.highscore_least_rounds = 0
+        self.highscores = Highscore()
 
     def reset_player_stats(self):
         """Beginning for an existing Player."""
         confirmation = input("Proceed with Reset? [y/N] ")
         confirmation.lower()
         if confirmation == "y":
-            self.highscore_least_rolls = 0
-            self.highscore_least_rounds = 0
+            self.highscores.reset_highscore()
 
     def change_name(self):
         """Alter the identity of the Player."""
@@ -35,3 +34,15 @@ class Player:
 
     def return_stats(self):
         """Museum of your previous games."""
+        lowest_roll, lowest_round = self.highscores.return_low_stats()
+        highest_points = self.highscores.return_high_stats()
+        wins, all_time_points = self.highscores.return_all_time_stats()
+        return lowest_roll, lowest_round, highest_points, wins, all_time_points
+
+    def update_scores(self, win):
+        """Update museum after finished game."""
+        rolls = self.dice_holder.get_amount_of_rolls()
+        rounds = self.dice_holder.get_total_points()
+        points = self.dice_holder.get_rounds()
+
+        self.highscores.update_highscore(rolls, rounds, points, win)
